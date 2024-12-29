d0db1f8473376baa891f9eff6c644fa3cd425cf3
LittleAdventurer <pyne469@gmail.com>
2024-12-29 16:10:18 +0630

    Clickable Views

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -26,12 +28,17 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true // Enable view binding
     }
 }
 
--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.colormyviews
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(
+            binding.boxOneText,
+            binding.boxTwoText,
+            binding.boxThreeText,
+            binding.boxFourText,
+            binding.boxFiveText,
+            binding.main
+        )
+        for (item in clickableViews) {
+            item.setOnClickListener {makeColored(it)}
         }
     }
+
+    fun makeColored(view: View) {
+        when (view) {
+            binding.boxOneText -> view.setBackgroundColor(Color.DKGRAY)
+            binding.boxTwoText -> view.setBackgroundColor(Color.GRAY)
+            binding.boxThreeText -> view.setBackgroundResource(android.R.color.holo_green_light)
+            binding.boxFourText -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            binding.boxFiveText -> view.setBackgroundResource(android.R.color.holo_green_light)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
 }
